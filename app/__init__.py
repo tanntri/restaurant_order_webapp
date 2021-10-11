7931afae041ed1cd18dbfc3cb65aa4ef1f6bba38
tann triyapanjawit <tann.triyapanjawit@gmail.com>
2021-10-11 01:29:54 -0700

    update postgres variable

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 # configure database
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///menu.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1",
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",
                                                        "sqlite:///menu.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app, session_options={"autoflush": False})
