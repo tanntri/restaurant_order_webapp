0faeffa0bccf84ad7c56f4c178b6254b6598fc2f
tann triyapanjawit <tann.triyapanjawit@gmail.com>
2021-10-11 21:16:24 -0700

    Working QR code and modify number field in order page

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -1,15 +1,16 @@
 import qrcode
 
-APP_ORDER_URL = 'https://restaurant-ordering-app.herokuapp.com/'
+APP_ORDER_URL = 'https://restaurant-order-app.herokuapp.com/order/'
 TABLE_AMOUNT = 15
 
-#Creating an instance of qrcode
-qr = qrcode.QRCode(version=1, box_size=10, border=5)
-
 for i in range(TABLE_AMOUNT):
+    #Creating an instance of qrcode
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
     input_data = f'{APP_ORDER_URL}{i+1}'
 
     qr.add_data(input_data)
     qr.make(fit=True)
     img = qr.make_image(fill='black', back_color='white')
     img.save(f'app/static/images/qrcode/qrcode{i+1}.png')
+
+    input_data = ''
